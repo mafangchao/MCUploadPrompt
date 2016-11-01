5fc11c3d853d38c4f9d19776d57303ac5c197d48
小马哥 <15618933468@163.com>
2016-11-01 17:46:12 +0800

    Initial Commit

---
+++ MCUploadPrompt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MCUploadPrompt
+//
+//  Created by 123 on 16/11/1.
+//  Copyright © 2016年 machao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
